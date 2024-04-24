a857340a61e0fc0097a8d920dd2c0a061236863d
sanketmp <sanketmpol@gmail.com>
2024-04-24 18:51:35 +0530

    fix precise location bug

--- script.js
+++ script.js
@@ -59,7 +59,16 @@ async function setWeatherFromCity() {
   let city = `q=${document.querySelector("#search-bar").value.toLowerCase()}`;
   let val = await fetch(`https://backend-wthr.vercel.app/${city}`);
   let data = await val.json();
-  updateweather(data);
+  if (document.querySelector("#search-bar").value === "") {
+    document.querySelector("#data").style.visibility = "visible";
+    document.querySelector("#headline").innerHTML =
+      "City cannot be blank: Says JS";
+  } else if (data?.cod == "404") {
+    document.querySelector("#data").style.visibility = "visible";
+    document.querySelector("#headline").innerHTML = "No City Found";
+  } else {
+    updateweather(data);
+  }
 }
 
 function time(timedata) {
@@ -84,61 +93,50 @@ function setHeadline(city) {
 }
 
 function updateweather(data) {
-  if (document.querySelector("#search-bar").value === "") {
-    document.querySelector("#data").style.visibility = "visible";
-    document.querySelector("#headline").innerHTML =
-      "City cannot be blank: Says JS";
-  } else if (data?.cod == "404") {
-    document.querySelector("#data").style.visibility = "visible";
-    document.querySelector("#headline").innerHTML = "No City Found";
-  } else {
-    document.querySelector("#data").style.visibility = "visible";
-    document.querySelector("#headline").innerHTML = setHeadline(data?.name);
-    document.querySelector("#cityname2").innerHTML = `${data?.name}`; //city name
-    document.querySelector("#main").innerHTML = `${data?.weather?.[0]?.main}`;
-    document.querySelector(
-      "#main-img"
-    ).src = `https://openweathermap.org/img/w/${data?.weather?.[0]?.icon}.png`;
-    document.querySelector("#main-temp").innerHTML = `${(
-      data?.main?.temp - 273
-    ).toFixed(1)}&deg;C`;
-    document.querySelector(
-      "#windspeed"
-    ).innerHTML = `${data?.wind?.speed}&nbsp;m&#47;s`;
-    document.querySelector(
-      "#windspeed2"
-    ).innerHTML = `${data?.wind?.speed}&nbsp;m&#47;s`;
-    document.querySelector(
-      "#humidity"
-    ).innerHTML = `${data?.main?.humidity}&nbsp;&percnt;`;
-    document.querySelector(
-      "#clouds"
-    ).innerHTML = `${data?.clouds?.all}&nbsp;&percnt;`;
-    document.querySelector("#mintemp").innerHTML = `${(
-      data?.main.temp_min - 273
-    ).toFixed(0)}`;
-    document.querySelector("#maxtemp").innerHTML = `${(
-      data?.main.temp_max - 273
-    ).toFixed(0)}`;
-    document.querySelector(
-      "#description"
-    ).innerHTML = `${data?.weather?.[0]?.description}`;
-    document.querySelector(
-      "#description2"
-    ).innerHTML = `${data?.weather?.[0]?.description}`;
-    document.querySelector("#feelslike").innerHTML = `${(
-      data?.main?.feels_like - 273
-    ).toFixed(0)}&deg;C`;
-    document.querySelector("#pressure").innerHTML = `${data?.main?.pressure}`;
-    document.querySelector("#ground").innerHTML = `${data?.main?.grnd_level}`;
-    document.querySelector("#winddegree").innerHTML = `${data?.wind?.deg}`;
-    document.querySelector("#sunrise").innerHTML = `${time(
-      data?.sys["sunrise"]
-    )}`;
-    document.querySelector("#sunset").innerHTML = `${time(
-      data?.sys["sunset"]
-    )}`;
-  }
+  document.querySelector("#data").style.visibility = "visible";
+  document.querySelector("#headline").innerHTML = setHeadline(data?.name);
+  document.querySelector("#cityname2").innerHTML = `${data?.name}`; //city name
+  document.querySelector("#main").innerHTML = `${data?.weather?.[0]?.main}`;
+  document.querySelector(
+    "#main-img"
+  ).src = `https://openweathermap.org/img/w/${data?.weather?.[0]?.icon}.png`;
+  document.querySelector("#main-temp").innerHTML = `${(
+    data?.main?.temp - 273
+  ).toFixed(1)}&deg;C`;
+  document.querySelector(
+    "#windspeed"
+  ).innerHTML = `${data?.wind?.speed}&nbsp;m&#47;s`;
+  document.querySelector(
+    "#windspeed2"
+  ).innerHTML = `${data?.wind?.speed}&nbsp;m&#47;s`;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `${data?.main?.humidity}&nbsp;&percnt;`;
+  document.querySelector(
+    "#clouds"
+  ).innerHTML = `${data?.clouds?.all}&nbsp;&percnt;`;
+  document.querySelector("#mintemp").innerHTML = `${(
+    data?.main.temp_min - 273
+  ).toFixed(0)}`;
+  document.querySelector("#maxtemp").innerHTML = `${(
+    data?.main.temp_max - 273
+  ).toFixed(0)}`;
+  document.querySelector(
+    "#description"
+  ).innerHTML = `${data?.weather?.[0]?.description}`;
+  document.querySelector(
+    "#description2"
+  ).innerHTML = `${data?.weather?.[0]?.description}`;
+  document.querySelector("#feelslike").innerHTML = `${(
+    data?.main?.feels_like - 273
+  ).toFixed(0)}&deg;C`;
+  document.querySelector("#pressure").innerHTML = `${data?.main?.pressure}`;
+  document.querySelector("#ground").innerHTML = `${data?.main?.grnd_level}`;
+  document.querySelector("#winddegree").innerHTML = `${data?.wind?.deg}`;
+  document.querySelector("#sunrise").innerHTML = `${time(
+    data?.sys["sunrise"]
+  )}`;
+  document.querySelector("#sunset").innerHTML = `${time(data?.sys["sunset"])}`;
 }
 
 document.querySelector("#today").innerHTML = `${todaydate()}`;
